1b77aae05d6a8ee0b3718807d868eddb736d3d29
hyperforest <rimbaerlangga@gmail.com>
2024-10-20 16:09:21 +0700

    remove query filter

--- queries/sample_and_split/03_resample_nodes_and_edges.sql
+++ queries/sample_and_split/03_resample_nodes_and_edges.sql
@@ -30,12 +30,6 @@ USING(
     sender_node_id
     , benef_node_id
 )
-WHERE 1 = 1
-    AND (
-        (e.data_split = 'train' AND sbp.count_trx_train > 0)
-        OR (e.data_split = 'valid' AND sbp.count_trx_valid > 0)
-        OR (e.data_split = 'test' AND sbp.count_trx_test > 0)
-    )
 ORDER BY
     trx_date
     , trx_id
