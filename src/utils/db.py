2e255ce497d9829ee7d696948122b05c2fa15bb0
hyperforest <rimbaerlangga@gmail.com>
2024-10-22 02:15:04 +0700

    add error handling for existing function

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,7 +1,13 @@
 import duckdb
 from .features import amount_encoding
 
+
 def load_db(db_path: str) -> duckdb.DuckDBPyConnection:
     conn = duckdb.connect(db_path)
-    conn.create_function("amount_encoding", amount_encoding, ["INT", "INT", "INT"], "FLOAT")
+    try:
+        conn.create_function(
+            "amount_encoding", amount_encoding, ["INT", "INT", "INT"], "FLOAT"
+        )
+    except duckdb.CatalogException:
+        pass
     return conn
