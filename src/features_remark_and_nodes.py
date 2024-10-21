2702b0172128899131d9f789a398950c7b8a6482
hyperforest <rimbaerlangga@gmail.com>
2024-10-22 02:14:42 +0700

    add precomputed embedding features

--- src/features_remark_and_nodes.py
+++ src/features_remark_and_nodes.py
@@ -2,6 +2,9 @@ import argparse
 import duckdb
 import sys
 import warnings
+import torch
+import pandas as pd
+from transformers import BertTokenizer, AutoModel
 
 sys.path.append("./utils/")
 
@@ -9,6 +12,8 @@ from utils.time_utils import timeit  # type: ignore
 from utils.db import load_db  # type: ignore
 
 warnings.filterwarnings("ignore")
+
+INDOBERT_MODEL = "indobert-lite-base-p2"
 
 
 @timeit
@@ -50,6 +55,112 @@ def create_days_features(
     conn.execute(query)
 
 
+def generate_embeddings(
+    conn: duckdb.DuckDBPyConnection,
+    word_list: list[str],
+    entity: str,
+    tokenizer,
+    text_encoder,
+    batch_size=1000,
+    table_name="embeddings",
+):
+    for i in range(0, len(word_list), batch_size):
+        print(i, i + batch_size)
+        word_list_batch = word_list[i : i + batch_size]
+        tokenized = tokenizer(
+            word_list_batch,
+            padding=True,
+            truncation=True,
+            max_length=32,
+            return_tensors="pt",
+        )
+
+        with torch.no_grad():
+            outputs = text_encoder(**tokenized)
+
+        embeddings = outputs.last_hidden_state[:, 0, :].numpy()
+        df_emb = pd.DataFrame(embeddings)
+        df_emb.insert(0, entity, word_list_batch)
+        df_emb = df_emb.rename(
+            columns={i: f"{entity}_emb_{i}" for i in range(len(df_emb.columns))}
+        )
+
+        if i == 0:
+            conn.sql(f"CREATE OR REPLACE TABLE {table_name} AS SELECT * FROM df_emb")
+        else:
+            conn.sql(f"INSERT INTO {table_name} SELECT * FROM df_emb")
+
+
+@timeit
+def create_remark_embeddings(
+    conn: duckdb.DuckDBPyConnection,
+    tokenizer,
+    text_encoder,
+    limit: int = 10000,
+    dest_table: str | None = "remark_embeddings",
+) -> None:
+    df = conn.sql(
+        """
+        SELECT
+            remark
+            , COUNT(trx_id)::INT32 AS cnt
+        FROM
+            edges
+        GROUP BY remark
+        ORDER BY cnt DESC
+        """
+    ).df()
+    df["pct"] = df["cnt"] / df["cnt"].sum()
+    df["csum"] = df["pct"].cumsum()
+    df = df.iloc[:limit]
+    remark_list = df["remark"].tolist()
+
+    generate_embeddings(
+        conn=conn,
+        word_list=remark_list,
+        entity="remark",
+        tokenizer=tokenizer,
+        text_encoder=text_encoder,
+        batch_size=1000,
+        table_name=dest_table,
+    )
+
+
+@timeit
+def create_node_name_embeddings(
+    conn: duckdb.DuckDBPyConnection,
+    tokenizer,
+    text_encoder,
+    limit: int = 10000,
+    dest_table: str | None = "node_name_embeddings",
+) -> None:
+    df = conn.sql(
+        """
+        SELECT
+            node_name
+            , COUNT(id)::INT32 AS cnt
+        FROM
+            nodes
+        GROUP BY node_name
+        ORDER BY cnt DESC
+        """
+    ).df()
+    df["pct"] = df["cnt"] / df["cnt"].sum()
+    df["csum"] = df["pct"].cumsum()
+    df = df.iloc[:limit]
+    node_list = df["node_name"].tolist()
+
+    generate_embeddings(
+        conn=conn,
+        word_list=node_list,
+        entity="node_name",
+        tokenizer=tokenizer,
+        text_encoder=text_encoder,
+        batch_size=1000,
+        table_name=dest_table,
+    )
+
+
 @timeit
 def get_features_statistics(
     conn: duckdb.DuckDBPyConnection,
@@ -68,12 +179,31 @@ def get_features_statistics(
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--db_path", type=str, required=True)
+    parser.add_argument("--limit_remark_embeddings", type=int, required=True)
+    parser.add_argument("--limit_node_name_embeddings", type=int, required=True)
     args = parser.parse_args()
+
+    tokenizer = BertTokenizer.from_pretrained(f"indobenchmark/{INDOBERT_MODEL}")
+    indobert = AutoModel.from_pretrained(f"indobenchmark/{INDOBERT_MODEL}")
 
     conn = load_db(args.db_path)
     create_remark_features(conn, dest_table="remark_features")
     create_node_trx_features(conn, dest_table="node_trx_features")
     create_days_features(conn, dest_table="days_features")
+    create_remark_embeddings(
+        conn,
+        tokenizer,
+        indobert,
+        dest_table="remark_embeddings",
+        limit=args.limit_remark_embeddings,
+    )
+    create_node_name_embeddings(
+        conn,
+        tokenizer,
+        indobert,
+        dest_table="node_name_embeddings",
+        limit=args.limit_node_name_embeddings,
+    )
     get_features_statistics(conn, dest_table="statistics_features")
 
 
--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -117,13 +117,16 @@ class CustomDataset(Dataset):
         )
 
     def _get_num_batches(self):
-        return self.conn.execute(
-            f"""
+        return (
+            self.conn.execute(
+                f"""
             SELECT MAX(batch_id) + 1 FROM batches WHERE data_split = '{self.data_split}'
             """
-        ).fetchone()[0] or 0
+            ).fetchone()[0]
+            or 0
+        )
 
-    @lru_cache(maxsize=2048)
+    @lru_cache(maxsize=1024)
     def _get_labels(self, idx):
         df_labels = self.conn.execute(
             f"""
@@ -154,7 +157,7 @@ class CustomDataset(Dataset):
 
         return tensor
 
-    @lru_cache(maxsize=2048)
+    @lru_cache(maxsize=1024)
     def _get_features(self, idx):
         df_features = self.conn.execute(
             f"""
@@ -171,6 +174,9 @@ class CustomDataset(Dataset):
             , features AS (
                 SELECT
                     b.trx_id
+                    , e.remark
+                    , node_s.node_name AS sender_node_name
+                    , node_b.node_name AS benef_node_name
                     , df.* EXCLUDE (calendar_date)
                     , amount_encoding(e.amount, 10_000, 100_000)         AS encoded_amount_10K_100K
                     , amount_encoding(e.amount, 100_000, 300_000)        AS encoded_amount_100K_300K
@@ -179,6 +185,9 @@ class CustomDataset(Dataset):
                     , amount_encoding(e.amount, 5_000_000, 10_000_000)   AS encoded_amount_5M_10M
                     , amount_encoding(e.amount, 10_000_000, 50_000_000)  AS encoded_amount_10M_50M
                     , amount_encoding(e.amount, 10_000, 50_000_000)      AS encoded_amount_10K_50M
+                    , re.* EXCLUDE (remark)
+                    , nne_s.* EXCLUDE (node_name)
+                    , nne_b.* EXCLUDE (node_name)
                 FROM
                     batch AS b
                 LEFT JOIN
@@ -186,10 +195,25 @@ class CustomDataset(Dataset):
                 LEFT JOIN
                     days_features AS df
                     ON e.trx_date = df.calendar_date
+                LEFT JOIN
+                    remark_embeddings AS re
+                    ON e.remark = re.remark
+                LEFT JOIN
+                    nodes AS node_s
+                    ON e.sender_node_id = node_s.id
+                LEFT JOIN
+                    nodes AS node_b
+                    ON e.benef_node_id = node_b.id
+                LEFT JOIN
+                    node_name_embeddings AS nne_s
+                    ON node_s.node_name = nne_s.node_name
+                LEFT JOIN
+                    node_name_embeddings AS nne_b
+                    ON node_b.node_name = nne_b.node_name
             )
 
             SELECT
-                * EXCLUDE(trx_id)
+                *
             FROM
                 features
             ORDER BY
@@ -197,60 +221,46 @@ class CustomDataset(Dataset):
             """
         ).df()
 
-        df_emb = self.conn.execute(
-            f"""
-            WITH batch AS (
-                SELECT
-                    trx_id
-                FROM
-                    batches
-                WHERE 1 = 1
-                    AND data_split = '{self.data_split}'
-                    AND batch_id = {idx}
-            )
+        # get df with no pre-computed embeddings
+        for emb_col, col in zip(
+            ["remark_emb_0", "node_name_emb_0", "node_name_emb_0_1"],
+            ["remark", "sender_node_name", "benef_node_name"],
+        ):
+            mask_null = df_features[emb_col].isnull()
+            df_no_emb = df_features.loc[mask_null, ["trx_id", col]]
 
-            SELECT
-                batch.trx_id
-                , s.node_name AS sender_node_name
-                , b.node_name AS benef_node_name
-                , e.remark
-            FROM
-                batch
-            LEFT JOIN
-                edges AS e USING(trx_id)
-            LEFT JOIN
-                nodes AS s ON e.sender_node_id = s.id
-            LEFT JOIN
-                nodes AS b ON e.benef_node_id = b.id
-            ORDER BY
-                trx_id
-            """
-        ).df()
+            if df_no_emb.empty:
+                continue
 
-        all_embeddings = []
+            embeddings = self.get_embeddings(df_no_emb[col].tolist())
+            len_emb = embeddings.shape[1]
+            splitted = emb_col.split("_")  # "{emb_col}_0_1" -> ["{emb_col}", "0", "1"]
+            idx_emb = splitted.index("emb")
+            emb_col_last = "_".join(splitted[: idx_emb + 1]) + f"_{len_emb - 1}"
+            if len(splitted) > idx_emb + 2:
+                emb_col_last += "_" + "_".join(splitted[idx_emb + 2 :])
 
-        for col in ["sender_node_name", "benef_node_name", "remark"]:
-            with torch.no_grad():
-                embeddings = self.text_encoder(
-                    **self.tokenizer(
-                        df_emb[col].tolist(),
-                        padding=True,
-                        truncation=True,
-                        max_length=32,
-                        return_tensors="pt",
-                    )
-                ).last_hidden_state[:, 0, :]
+            df_features.loc[mask_null, emb_col:emb_col_last] = embeddings.numpy()
 
-            all_embeddings.append(embeddings)
-
-        all_embeddings = torch.cat(all_embeddings, dim=1)
-        features = torch.cat(
-            [torch.tensor(df_features.values, dtype=torch.float32), all_embeddings],
-            dim=1,
+        features = df_features.drop(
+            columns=["trx_id", "remark", "sender_node_name", "benef_node_name"]
         )
-
+        features = torch.tensor(features.values, dtype=torch.float32)
         return features
 
-    @lru_cache(maxsize=2048)
+    @lru_cache(maxsize=1024)
     def __getitem__(self, idx):
         return self._get_features(idx), self._get_labels(idx)
+
+    def get_embeddings(self, word_list):
+        with torch.no_grad():
+            embeddings = self.text_encoder(
+                **self.tokenizer(
+                    word_list,
+                    padding=True,
+                    truncation=True,
+                    max_length=32,
+                    return_tensors="pt",
+                )
+            ).last_hidden_state[:, 0, :]
+        return embeddings
